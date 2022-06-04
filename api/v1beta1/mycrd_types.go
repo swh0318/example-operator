d16dc639ca6863fb66325ac74fa942a36a9f6605
sunwenhua <sunwenhua@haodf.com>
2022-06-04 22:13:15 +0800

    提交自定义内容

--- api/v1beta1/mycrd_types.go
+++ api/v1beta1/mycrd_types.go
@@ -24,24 +24,28 @@ import (
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
 // MyCRDSpec defines the desired state of MyCRD
+// TODO(user):用户自定义逻辑
 type MyCRDSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
-
-	// Foo is an example field of MyCRD. Edit mycrd_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	DeploymentName string `json:"deploymentName"`
+	Replicas       *int32 `json:"replicas"`
 }
 
 // MyCRDStatus defines the observed state of MyCRD
+// TODO(user):用户自定义逻辑
 type MyCRDStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+	// +optional
+	AvailableReplicas int32 `json:"availableReplicas"`
 }
 
 //+kubebuilder:object:root=true
 //+kubebuilder:subresource:status
 
 // MyCRD is the Schema for the mycrds API
+// TODO(user):用户自定义逻辑
 type MyCRD struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
--- controllers/mycrd_controller.go
+++ controllers/mycrd_controller.go
@@ -18,13 +18,20 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/klog/v2"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
+	"github.com/pingcap/errors"
 	crdv1beta1 "github.com/swh0318/sample-operator/api/v1beta1"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // MyCRDReconciler reconciles a MyCRD object
@@ -33,7 +40,10 @@ type MyCRDReconciler struct {
 	Scheme *runtime.Scheme
 }
 
+// TODO(user):用户自定义逻辑
 //+kubebuilder:rbac:groups=crd.github.com,resources=mycrds,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=crd.github.com,resources=pods,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=crd.github.com,resources=deployments,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=crd.github.com,resources=mycrds/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=crd.github.com,resources=mycrds/finalizers,verbs=update
 
@@ -43,14 +53,52 @@ type MyCRDReconciler struct {
 // the MyCRD object against the actual cluster state, and then
 // perform operations to make the cluster state reflect the state specified by
 // the user.
-//
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.11.2/pkg/reconcile
 func (r *MyCRDReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	_ = log.FromContext(ctx)
+	// TODO(user):用户自定义逻辑
+	myCRD := &crdv1beta1.MyCRD{}
+	if err := r.Get(ctx, req.NamespacedName, myCRD); err != nil {
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
 
-	// TODO(user): your logic here
-
+	deploymentName := myCRD.Spec.DeploymentName
+	if deploymentName == "" {
+		return ctrl.Result{}, fmt.Errorf("%s: deployment name must be specified", myCRD.GetGenerateName)
+	}
+	deployment := &appsv1.Deployment{}
+	err := r.Get(ctx, req.NamespacedName, deployment)
+	if err == nil {
+		if !metav1.IsControlledBy(deployment, myCRD) {
+			msg := fmt.Sprintf("deployment is controlled by myCRD: %s", deployment.Name)
+			return ctrl.Result{}, fmt.Errorf("%s", msg)
+		}
+		if myCRD.Spec.Replicas != nil && *myCRD.Spec.Replicas != *deployment.Spec.Replicas {
+			klog.Infof("============mycrd replicas not equal deployment replicas===========")
+			newDeployment := r.newDeployment(myCRD)
+			if err := r.Update(ctx, newDeployment); err != nil && !errors.IsAlreadyExists(err) {
+				return ctrl.Result{}, err
+			}
+		}
+		// 更新CRD状态
+		myCRD.Status.AvailableReplicas = deployment.Status.AvailableReplicas
+		if err := r.Status().Update(ctx, myCRD); err != nil {
+			return ctrl.Result{}, err
+		}
+	} else if err != nil && errors.IsNotFound(err) {
+		// 创建MyCRD对应的deployment
+		deployment := r.newDeployment(myCRD)
+		if err := controllerutil.SetControllerReference(myCRD, deployment, r.Scheme); err != nil {
+			return ctrl.Result{}, err
+		}
+		if err := r.Create(ctx, deployment); err != nil && !errors.IsAlreadyExists(err) {
+			return ctrl.Result{}, err
+		}
+		klog.Infof("===========mycrd create deloyment success!!============")
+	} else {
+		return ctrl.Result{}, err
+	}
 	return ctrl.Result{}, nil
 }
 
@@ -59,4 +107,40 @@ func (r *MyCRDReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&crdv1beta1.MyCRD{}).
 		Complete(r)
+}
+
+// newDeployment creates a new Deployment for a MyCRD resource
+func (r *MyCRDReconciler) newDeployment(myCRD *crdv1beta1.MyCRD) *appsv1.Deployment {
+	labels := map[string]string{
+		"app":        "mycrd",
+		"controller": "mycrd",
+	}
+	return &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      myCRD.Spec.DeploymentName,
+			Namespace: myCRD.Namespace,
+			// OwnerReferences: []metav1.OwnerReference{
+			// 	*metav1.NewControllerRef(myCRD, v1beta1.SchemeGroupVersion.WithKind("MyCRD")),
+			// },
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: myCRD.Spec.Replicas,
+			Selector: &metav1.LabelSelector{
+				MatchLabels: labels,
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: labels,
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:  "doraemon-frontend",
+							Image: "360cloud/doraemon-frontend",
+						},
+					},
+				},
+			},
+		},
+	}
 }
